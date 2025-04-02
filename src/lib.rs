ff192c8495321ada952a37b3bff5c4b286f732b1
Emad Akhras <emadakhrasidleb@gmail.com>
2025-04-02 10:18:20 +0800

    Trait to generate more questions based on its style

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 use std::marker;
 
+pub trait Quizzer {
+    fn generate(old: &[&Self]) -> Self
+    where
+        Self: Sized;
+}
+
 pub trait Solver<T: Eq + PartialEq> {
     fn solve(&self) -> T;
 }
@@ -7,13 +13,13 @@ pub trait Solver<T: Eq + PartialEq> {
 pub struct Answered<T: Eq + PartialEq>(T);
 pub struct Unanswered;
 
-pub struct Question<T: Eq + PartialEq, S: Solver<T>, State = Unanswered> {
+pub struct Question<T: Eq + PartialEq, S: Solver<T> + Quizzer, State = Unanswered> {
     style: S,
     state: State,
     _phantom: marker::PhantomData<T>,
 }
 
-impl<T: Eq + PartialEq, S: Solver<T>> Question<T, S, Unanswered> {
+impl<T: Eq + PartialEq, S: Solver<T> + Quizzer> Question<T, S, Unanswered> {
     fn new(style: S) -> Self {
         Self {
             style,
@@ -31,18 +37,18 @@ impl<T: Eq + PartialEq, S: Solver<T>> Question<T, S, Unanswered> {
     }
 }
 
-impl<T: Eq + PartialEq, S: Solver<T>> Question<T, S, Answered<T>> {
+impl<T: Eq + PartialEq, S: Solver<T> + Quizzer> Question<T, S, Answered<T>> {
     fn mark(&self) -> bool {
         self.state.0 == self.style.solve()
     }
 }
 
 #[derive(Default)]
-pub struct Section<T: Eq + PartialEq, S: Solver<T>, State = Unanswered> {
+pub struct Section<T: Eq + PartialEq, S: Solver<T> + Quizzer, State = Unanswered> {
     questions: Vec<Question<T, S, State>>,
 }
 
-impl<T: Eq + PartialEq, S: Solver<T>> Section<T, S, Unanswered> {
+impl<T: Eq + PartialEq, S: Solver<T> + Quizzer> Section<T, S, Unanswered> {
     pub fn new() -> Self {
         Self { questions: vec![] }
     }
