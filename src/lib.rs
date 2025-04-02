5be04ed8c915e4f59f77e7a8825fa2f47526d2ab
Emad Akhras <emadakhrasidleb@gmail.com>
2025-04-02 10:06:14 +0800

    Introduce generic style field into question

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,32 @@
+use std::marker;
+
+pub trait Solver<T: Eq + PartialEq> {
+    fn solve(&self) -> T;
+}
+
 pub struct Answered<T: Eq + PartialEq>(T);
 pub struct Unanswered;
 
-pub struct Question<State = Unanswered> {
+pub struct Question<T: Eq + PartialEq, S: Solver<T>, State = Unanswered> {
+    style: S,
     state: State,
+    _phantom: marker::PhantomData<T>,
 }
 
-impl Question<Unanswered> {
-    fn new() -> Self {
-        Self { state: Unanswered }
+impl<T: Eq + PartialEq, S: Solver<T>> Question<T, S, Unanswered> {
+    fn new(style: S) -> Self {
+        Self {
+            style,
+            state: Unanswered,
+            _phantom: marker::PhantomData::<T>,
+        }
     }
 
-    fn answer<T: Eq + PartialEq>(self, answer: T) -> Question<Answered<T>> {
+    fn answer(self, answer: T) -> Question<T, S, Answered<T>> {
         Question {
+            style: self.style,
             state: Answered(answer),
+            _phantom: marker::PhantomData::<T>,
         }
     }
 }
