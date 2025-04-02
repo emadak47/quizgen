aeb72ff6c598f7c3382d721b72b758a8e1d0d69b
Emad Akhras <emadakhrasidleb@gmail.com>
2025-04-02 10:58:59 +0800

    Pretty debug and display of multiple choice question

--- src/mcq.rs
+++ src/mcq.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub enum Choice {
     A,
@@ -12,4 +14,53 @@ pub struct MCQ {
     sentence: String,
     choices: [String; 4],
     solution: Choice,
+}
+
+impl MCQ {
+    fn solution(&self) -> &String {
+        &self.choices[self.solution as usize]
+    }
+}
+
+impl fmt::Debug for MCQ {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(
+            f,
+            "{}",
+            self.sentence.replacen(self.solution(), "[.....]", 1)
+        )?;
+        writeln!(f)?;
+
+        for (idx, choice) in self.choices.iter().enumerate() {
+            writeln!(
+                f,
+                "\t{}. {} {}",
+                (b'A' + idx as u8) as char,
+                if idx == self.solution as usize {
+                    "✓"
+                } else {
+                    " "
+                },
+                choice
+            )?;
+        }
+        Ok(())
+    }
+}
+
+impl fmt::Display for MCQ {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(
+            f,
+            "{}",
+            self.sentence.replacen(self.solution(), "[.....]", 1)
+        )?;
+        writeln!(f)?;
+
+        for (idx, choice) in self.choices.iter().enumerate() {
+            writeln!(f, "\t{}. {}", (b'A' + idx as u8) as char, choice)?;
+        }
+
+        Ok(())
+    }
 }
